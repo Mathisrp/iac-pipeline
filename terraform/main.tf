2dbadd5859b0b0d3c86715fa0796f6297821fe60
Mathisrp <91907502+Mathisrp@users.noreply.github.com>
2025-09-15 12:44:21 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,13 +13,12 @@ provider "aws" {
 variable "ami_id" {
   description = "AMI utilisée pour créer l'instance"
   type        = string
-  default     = "ami-initial"
 }
-
 
 resource "aws_instance" "demo" {
   ami           = var.ami_id
   instance_type = "t2.micro"
+
   tags = {
     Name = "Instance-${var.ami_id}"
   }
